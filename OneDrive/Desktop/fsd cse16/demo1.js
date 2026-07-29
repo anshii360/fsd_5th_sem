f073015beec6deb19e43d0744c250aa8c64f0b9f
anshii360 <anshiiiiii07@gmail.com>
2026-07-29 22:00:23 +0530

    initial commit

---
+++ OneDrive/Desktop/fsd cse16/demo1.js
@@ -0,0 +1,3 @@
+var a=10;
+var a=15;
+console.log("A=",a)
---
+++ OneDrive/Desktop/fsd cse16/demo4.js
@@ -0,0 +1,11 @@
+const student={
+    name:"Anshika",
+    age:18,
+    branch:"CSE"
+}
+const name=student.name;
+const age=student.age;
+const branch=student.branch;
+console.log("name=",name)
+console.log("age=",age)
+console.log("branch=",branch)
